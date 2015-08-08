ea0f5fd0f08bfadfe0684f93399be01bb7125bd4
Linus Wallgren <linus.wallgren@gmail.com>
2015-08-08 13:52:58 +0200

    Add config file support
    The input names are now read from ~/.marantzrc instead of being
    hardcoded

--- src/python/main.py
+++ src/python/main.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import argparse
+from configparser import ConfigParser
 import itertools
+import os
 
 from modules.Input import InputModule
 from modules.Power import PowerModule
 from modules.Status import StatusModule
 from modules.Volume import VolumeModule
 
+config = ConfigParser()
+config.read(os.path.expanduser('~/.marantzrc'))
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest='action')
 
 
-modules = [InputModule(), VolumeModule(), PowerModule(), StatusModule()]
+modules = [InputModule(config), VolumeModule(), PowerModule(), StatusModule()]
 module_actions = map(lambda module: module.get_actions(), modules)
 actions = itertools.chain(*module_actions)
 
--- src/python/modules/Input.py
+++ src/python/modules/Input.py
@@ -2,26 +2,29 @@ from action.Action import Action
 from marantz import send_command
 from modules.ActionModule import ActionModule
 
-INPUT_MAPPING = {
-    'saga': 'MPLAY',
-    'xbox': 'GAME'
-}
-
-
-def set_input(source_name):
-    if source_name not in INPUT_MAPPING:
-        print('No such input: ' + source_name)
-        return
-    send_command('PutZone_InputFunction', INPUT_MAPPING[source_name])
-
 
 class Input(Action):
+    def __init__(self, input_names):
+        self.input_names = input_names
+
+    def _set_input(self, source_name):
+        if source_name not in self.input_names:
+            print('No such input: ' + source_name)
+            return
+        send_command('PutZone_InputFunction', self.input_names[source_name])
+
     def add_parser(self, subparsers):
         inputparser = subparsers.add_parser('input')
-        inputparser.add_argument('input', choices=INPUT_MAPPING.keys())
-        inputparser.set_defaults(func=lambda args: set_input(args.input))
+        inputparser.add_argument('input', choices=self.input_names.keys())
+        inputparser.set_defaults(func=lambda args: self._set_input(args.input))
 
 
 class InputModule(ActionModule):
+    input_names = {}
+
+    def __init__(self, config):
+        if 'INPUTS' in config:
+            self.input_names = config['INPUTS']
+
     def get_actions(self):
-        return [Input()]
+        return [Input(self.input_names)]
