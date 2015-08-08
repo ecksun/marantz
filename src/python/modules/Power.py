e18dbc5360efc54b7e7ce739efc325f23b094e93
Linus Wallgren <linus.wallgren@gmail.com>
2015-08-08 15:53:11 +0200

    Ensure power is on before continuing in on action
    Previously if we sent to many commands (as we did when turning on all
    zones) some was ignored. We now try again if the state doesn't change
    fast enough

--- src/python/modules/Power.py
+++ src/python/modules/Power.py
@@ -1,7 +1,10 @@
 from enum import Enum
+import time
+
 from action.SimpleAction import SimpleAction
 from marantz import send_command
 from modules.ActionModule import ActionModule
+from modules.Status import get_status
 from zone import Zones
 
 
@@ -13,6 +16,9 @@ class Power(Enum):
 def power(given_zone, state: Power):
     for zone in Zones.get_zones(given_zone):
         send_command(zone, 'PutZone_OnOff', state.value)
+        while state is Power.ON and get_status(zone)['zonepower'] is False:
+            time.sleep(0.5)
+            send_command(zone, 'PutZone_OnOff', state.value)
 
 
 class PowerModule(ActionModule):
