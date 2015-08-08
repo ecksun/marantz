79950de910ae982e9db31d2c3b512625d2d0b028
Linus Wallgren <linus.wallgren@gmail.com>
2015-08-08 15:08:20 +0200

    Add support for --all flag for input action

--- src/python/modules/Input.py
+++ src/python/modules/Input.py
@@ -1,22 +1,24 @@
 from action.Action import Action
 from marantz import send_command
 from modules.ActionModule import ActionModule
+from zone import Zones
 
 
 class Input(Action):
     def __init__(self, input_names):
         self.input_names = input_names
 
-    def _set_input(self, zone, source_name):
+    def _set_input(self, zones, source_name):
         if source_name not in self.input_names:
             print('No such input: ' + source_name)
             return
-        send_command(zone, 'PutZone_InputFunction', self.input_names[source_name])
+        for zone in zones:
+            send_command(zone, 'PutZone_InputFunction', self.input_names[source_name])
 
     def add_parser(self, subparsers):
         inputparser = subparsers.add_parser('input')
         inputparser.add_argument('input', choices=self.input_names.keys())
-        inputparser.set_defaults(func=lambda args: self._set_input(args.zone, args.input))
+        inputparser.set_defaults(func=lambda args: self._set_input(Zones.get_zones(args.zone), args.input))
 
 
 class InputModule(ActionModule):
